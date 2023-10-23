87337d94a51cd3e691f69b36d249467e9e0f01e9
Mahabubur Rahman <mahabubmon@gmail.com>
2023-10-23 14:27:54 +0600

    adding_html

---
+++ portfolio/src/views/pages/Master.vue
@@ -0,0 +1,84 @@
+<template>
+    <div>
+        <div class="container">
+  <header>
+    <nav class="navbar navbar-expand-md">
+      
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span><i class="fas fa-bars navbar-icon custom-icon"></i></span>
+      </button>
+      
+      <a class="navbar-brand" href="#" >
+        <img src="image/logo2.png" alt="">
+        <h5 class="logo-icon"><br>Md. Mahabubur Rahman</h5>
+      </a>
+    
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="index.html">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="about.html">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="contect.html">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    
+    <section class="hero">
+      <div class="container">
+          <div class="row align-items-center">
+              <div class="col-md-6" id="">
+                  <h2>Welcome to My Portfolio</h2>
+                  <p>I'm Md. Mahabubur Rahman, a passionate web developer 
+                    <br>To make the most out of the opportunities I receive and to utilize my full potential as a creative
+                    thinker and developer. Always eager to learn new things and face new challenges . A team player
+                    and motivator
+                    <br> I'm a entry level html,css,bootstrap and javascript programmer.
+                    <br>I do have plan to become a full stack web developer.
+                    
+                  </p>
+              </div>
+              <div class="col-md-6">
+                  <img src="image/myimage2.jpeg" alt="Your Image" class="img-fluid hero-img">
+              </div>
+          </div>
+      </div>
+  </section>
+
+  <section class="projects">
+    <h1>Notable Projects and Achievements</h1>
+  <div class="project-container">
+    <div class="project">
+      <img src="image/todo.png" alt="Project 1">
+      <h3>Todo app</h3>
+      <p>These apps have become popular tools for individuals, students, professionals, and teams to boost productivity, improve time management, and ensure tasks are completed efficiently. With a todo app, users can better stay on top of their responsibilities, set goals, and achieve their objectives more effectively.</p>
+    </div>
+    <div class="project">
+      <img src="image/slide-show.png" alt="Project 2">
+      <h3>Slide-show</h3>
+      <p>Slideshow presentations are widely used in a range of settings, from business meetings and academic presentations to personal events like weddings or family gatherings. They are an effective way to showcase content, deliver information, and engage the audience visually.</p>
+    </div>
+    <div class="project">
+      <img src="image/guessing-game.png" alt="Project 3">
+      <h3>Guessing-game</h3>
+      <p>Guessing games can vary in complexity and can be adapted for different age groups and occasions. They are commonly used as icebreaker activities, entertainment at parties, or as educational tools to reinforce learning in a fun and interactive way.</p>
+    </div>
+    
+  </div>
+  </section>
+  </header>
+</div>
+    </div>
+</template>
+
+<script>
+export default{
+    name:'Masterf',
+}
+
+
+</script>
